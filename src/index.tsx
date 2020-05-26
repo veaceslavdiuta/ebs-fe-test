e1deb0661a3df6975fcbb8f32eecbc095600f524
Vladimir Josan <vladimir.josan@ebs-integrator.com>
2020-05-26 15:07:33 +0300

    fixed nested links (4)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
-import "./index.css";
-
-render(<div>Hello World!</div>, document.getElementById("root"));
+render(<div>Hello World!</div>, document.getElementById('root'));
