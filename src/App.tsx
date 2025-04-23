47263e7231399d078dae872ad8da230a1c0a71f7
Veaceslav Diuta <veaceslavdiuta.dev@gmail.com>
2025-04-23 14:52:39 +0300

    initial setup: install dependencies and create base folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to the Front-End Developer Test!</h1>
-      <p>We wish you the best of luck. Please make sure to read the README file for
-        instructions.</p>
-    </div>
-  )
+  return <div className=""></div>;
 }
 
-export default App
+export default App;
